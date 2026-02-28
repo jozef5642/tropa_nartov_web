f8daededd876b8bc79cdabefe6f2148741ae941a
Jozeph <jozeppe1997@gmail.com>
2026-02-28 16:10:13 +0300

    versel install

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,13 @@ import Section_3 from "@/components/section_3/Section_3";
 import Section_4 from "@/components/section_4/Section_4";
 import Section_5 from "@/components/section_5/Section_5";
 import Section_6 from "@/components/section_6/Section_6";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 /** Главная страница — контент в div_container */
 export default function Home() {
   return (
+    <>
+      <SpeedInsights />
     <div className={styles.div_container_background}>
       <div className={styles.div_container}>
         <Section_1 />
@@ -19,5 +22,6 @@ export default function Home() {
         <Section_6 />
       </div>
     </div>
+    </>
   );
 }
